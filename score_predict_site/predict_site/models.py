99d2d22ab2f8e460bebfdb5291f7126c78dccc07
sshhhll <sshhhll@sina.com>
2019-02-11 15:54:34 +0800

    主要修改数据库Models.py

--- score_predict_site/predict_site/models.py
+++ score_predict_site/predict_site/models.py
@@ -12,8 +12,23 @@ class Users(models.Model):
     portrait = models.ImageField(upload_to="static/user_portrait")
     mail = models.CharField(u"邮箱", max_length=100)
     status = models.CharField(u"状态/个性签名", max_length=255, null=True)
-    Uphy = models.CharField(u"大学物理", max_length=100)
-    Amath = models.CharField(u"高等数学", max_length=100)
-    Lalg = models.CharField(u"线性代数", max_length=100)
-    C = models.CharField(u"C语言", max_length=100)
-    Cpp = models.CharField(u"C++", max_length=100)
+    Uphy = models.CharField(u"大学物理", max_length=100, null=True)
+    Amath = models.CharField(u"高等数学", max_length=100, null=True)
+    Lalg = models.CharField(u"线性代数", max_length=100, null=True)
+    C = models.CharField(u"C语言", max_length=100, null=True)
+    Cpp = models.CharField(u"C++", max_length=100, null=True)
+
+
+class Courses(models.Model):
+    name = models.CharField(u"课程名", max_length=100)
+    intro = models.TextField(u"课程简介")
+    # lesson_hours = models.FloatField(u"课时", null=True)
+    course_id = models.IntegerField(u"课程名id", null=True)  # 课程名在图数据库中的id
+
+
+class StuScore(models.Model):
+    score_predict = models.FloatField(u"预测分数", null=True)
+    score_actual = models.FloatField(u"实际分数", null=True)
+    stu_no = models.CharField(u"学号", max_length=100)
+    course_id = models.IntegerField(U"课程名id")
+    predict_accuracy_rating = models.FloatField(u"预测准确度", null=True)
